baa283cd23c2e25395b680b85b155408ccb3a6cc
Stephany Lopes <stephanylopesfabricio@gmail.com>
2023-10-05 16:22:16 -0300

    Criando o JavaScript

---
+++ js/js.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.getElementById('search-button');
+    const cepInput = document.getElementById('cep-input');
+    const addressInfo = document.getElementById('address-info');
+
+    searchButton.addEventListener('click' , () => {
+        const cep = cepInput.value.replace(/\D/g, ''); //Remove caracteres não numéricos
+        
+        if (cep.leght !== 8) {
+            alert('Por favor, insira um Cep válido com 8 dígitos.');
+            return;
+        }
+
+        const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                alert('CEP não encontrado. Verifique o CEP digitado.');
+              }else {
+                const addressHTML = `
+                <h2>Endereço Encontrado:</h2>
+                <p>CEP: ${data.cep}</p>
+                <p>Logradouro: ${data.logradouro}</p>
+                <p>Complemento: ${data.complemento}</p> 
+                <p>Bairro: ${data.bairro}</p>
+                <p>Cidade/UF: ${data.localidade}/${data.uf}</p>`;
+
+                addressInfo.innerHTML = addressHTML;
+              }
+        })
+    .catch(error => {
+        console.error('Erro ao buscar o endereço:', error);
+        alert('Não foi possivel obter o endereço. Verifique sua conexão com a internet');
+
+    })
+    })
+})
